f073bfddb573c82202af38e5cbe62338206d3ea4
danale <danale@danales-MacBook-Pro.local>
2017-02-08 23:03:51 -0500

    Developed List of Items with ListView

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 import React, {Component} from "react";
-import {View, Text, StyleSheet, Platform} from "react-native";
+import {View, Text, StyleSheet, Platform, ListView, Keyboard} from "react-native";
 import Header from "./header";
 import Footer from "./footer";
+import Row from "./row";
 
 class App extends Component {
 	constructor(props) {
 		super(props);
+		// ListView - A core component designed for efficient display of vertically
+		// scrolling lists of changing data. The minimal API is to create a ListView.DataSource,
+		// populate it with a simple array of data blobs, and instantiate a ListView component with
+		// that data source and a renderRow callback which takes a blob from the data array and returns
+		// a renderable component.
+		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 		this.state = {
 			allComplete: false,
 			value: "",
-			items: []
+			items: [],
+			dataSource: ds.cloneWithRows([])
 		}
+		this.setSource = this.setSource.bind(this);
 		this.handleAddItem = this.handleAddItem.bind(this);
 		this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
+	}
+	setSource(items, itemsDatasource, otherState = {}) {
+		this.setState({
+			items,
+			dataSource: this.state.dataSource.cloneWithRows(itemsDatasource),
+			...otherState
+		})
 	}
 	handleToggleAllComplete() {
 		const complete = !this.state.allComplete;
@@ -20,11 +36,7 @@ class App extends Component {
 			...item,
 			complete
 		}))
-		console.table(newItems);
-		this.setState({
-			items: newItems,
-			allComplete: complete
-		})
+		this.setSource(newItems, newItems, {allComplete: complete})
 	}
 	handleAddItem() {
 		if(!this.state.value) return;
@@ -36,18 +48,35 @@ class App extends Component {
 				complete: false
 			}
 		]
-		this.setState({
-			items: newItems,
-			value: ""
-		})
+		this.setSource(newItems, newItems, {value: ""})
 	}
 	render() {
 		return (
 			<View style={styles.container}>
-				<Header value={this.state.value} onAddItem={this.handleAddItem} onChange={(value) => this.setState({value})}
-					onToggleAllComplete={this.handleToggleAllComplete} />
+				<Header
+					value={this.state.value}
+					onAddItem={this.handleAddItem}
+					onChange={(value) => this.setState({value})}
+					onToggleAllComplete={this.handleToggleAllComplete}
+				/>
 					<View style={styles.content}>
-
+						<ListView
+							style={styles.list}
+							enableEmptySections
+							dataSource={this.state.dataSource}
+							onScroll={() => Keyboard.dismiss()}
+							renderRow={({key, ...value}) => {
+								return (
+									<Row
+										key={key}
+										{...value}
+									/>
+								)
+							}}
+							renderSeparator={(sectionId, rowId) => {
+								return <View key={rowId} style={styles.separator}/>
+							}}
+						/>
 					</View>
 				<Footer />
 			</View>
@@ -65,6 +94,13 @@ const styles = StyleSheet.create({
 	},
 	content: {
 		flex: 1,
+	},
+	list: {
+		backgroundColor: '#FFF'
+	},
+	separator: {
+		borderWidth: 1,
+		borderColor: "#F5F5F5"
 	}
 })
 
